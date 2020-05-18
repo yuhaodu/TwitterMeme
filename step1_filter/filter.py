df0338d073c3e8edd8e1cdb35bca341db2dc798b
yuhaodu <yuhaodu@buffalo.edu>
2020-05-18 16:36:52 +0000

    update

--- step1_filter/filter.py
+++ step1_filter/filter.py
@@ -4,7 +4,9 @@ import os
 import argparse
 import pickle
 import text_preprocessing as tp
-
+import regex as re
+import shutil
+from tqdm import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_dir', default='')
 args = parser.parse_args()
@@ -12,8 +14,8 @@ input_d = args.input_dir
 os.mkdir('../data/Image_with_Text')
 image_list = os.listdir(input_d)
 text_list = []
-name_lsit = []
-for i in image_list:
+name_list = []
+for i in tqdm(image_list):
     
     img = Image.open(os.path.join(input_d,i))
     text = image_to_string(img)
@@ -24,7 +26,7 @@ for i in image_list:
         shutil.move(os.path.join(input_d,i),os.path.join('../data/Image_with_Text',i))
 
 text_list = tp.preprocess(text_list)
-dict_name_text = {*zip(name_list,text_list)}
+dict_name_text = dict(zip(name_list,text_list))
 pickle.dump(dict_name_text,open('../data/name_text.pkl','wb'))
 
         
---
+++ step2_MemeClassifier/classification.py
@@ -0,0 +1,73 @@
+"""
+This is a file to make classification for IWT memes
+"""
+import torch
+import torch.nn as nn
+import json
+import parameters as p
+import os
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils, datasets, models
+import numpy as np
+import pickle
+import MemeDataset as md
+import MemeModel as mm
+import train_test as tt
+import csv
+import matplotlib.pyplot as plt
+import help as hp
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+from collections import OrderedDict
+from shutil import copyfile
+from zipfile import ZipFile
+
+input_dir = p.input_dir
+threshold = p.threshold
+glove_dir = p.glove_dir
+dict_dir = p.dict_dir
+model_dir = p.model_dir
+
+with ZipFile(glove_dir,'r') as zip:
+    zip.extract('glove.6B.50d.txt',path = '../data')
+
+vectors = []
+words = []
+word2idx = {}
+with open("../data/glove.6B.50d.txt", 'r') as f:
+    for index,line in enumerate(f):
+        values = line.split()
+        words.append(values[0])
+        vector = np.asarray(values[1:], "float32")
+        vectors.append(vector)
+        word2idx[values[0]] = index
+
+matrix = torch.FloatTensor(vectors)
+resnet = models.resnet50(pretrained = True) # load pretrained the resnet50 model
+backbone = nn.Sequential(*list(resnet.children())[:-1]) 
+
+trans = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),transforms.Normalize(mean=p.mean,std=p.std)])
+
+# load test_dataset
+dict_=pickle.load(open(dict_dir,'rb'))
+test_dataset = md.MemeDataset(input_dir,word2idx,dict_,p.max_len,transform = trans)
+model = mm.MemeClassifier(backbone,matrix)
+
+gpu = True # use GPU or not
+checkpoint = torch.load(model_dir)
+model.load_state_dict(checkpoint['model_state'])
+IWTmeme_name, nonIWTmeme_name = tt.test(model,gpu,test_dataset,threshold)
+
+try:
+    os.mkdir('../data/IWTmeme')
+except:
+    os.rmdir('../data/IWTmeme')
+    os.mkdir('../data/IWTmeme')
+try:
+    os.mkdir('../data/nonIWTmeme')
+except:
+    os.rmdir('../data/nonIWTmeme')
+    os.mkdir('../data/nonIWTmeme')
+for i in IWTmeme_name:
+    copyfile(os.path.join(test_root,i),os.path.join('./IWTmeme',i))
+for i in nonIWTmeme_name:      copyfile(os.path.join(test_root,i),os.path.join('./nonIWTmeme',i))
